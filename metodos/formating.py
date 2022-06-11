fbf0aa394d407a711630ddf5c2dd3d60cc3f019f
Rodrigo Hernandez <rodrigoisraelaa@gmail.com>
2022-06-11 18:19:23 -0500

    se cambio de leer exceles a leer csv

--- metodos/formating.py
+++ metodos/formating.py
@@ -10,6 +10,8 @@ def formatingpausas(x):
     y = np.append(np.append(x[:29, :7], x[:29, 7:14]), x[:15, 14:21])
     y = np.reshape(y, (73, 7))
     y = np.delete(y, 1, axis=1)
+    for x in y:
+        x[0] = (int(x[0][:-3])+(int(x[0][-2:])/60))/24
     return y
 
 
@@ -18,8 +20,7 @@ def formatingprogram(x):
     x = x[4:]
     y = []
     for t in x[:, 0]:
-        h = t.strftime("%H:%M")
-        u = (float(h[:2])+float(h[-2:])/60)/24
+        u = (float(t[:-3]) + float(t[-2:]) / 60) / 24
         y.append(u)
     x[:, 0] = y
     return x
